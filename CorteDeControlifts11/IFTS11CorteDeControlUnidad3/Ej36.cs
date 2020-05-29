15eeb14448708353afc578335bcd7a5a0a8e8e9d
Juan Emiliano Cicirello <juanemilianocicirello@gmail.com>
2020-05-29 17:28:28 -0300

    Ejercicio

---
+++ CorteDeControlifts11/IFTS11CorteDeControlUnidad3/Ej36.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IFTS11CorteDeControlUnidad3
+{
+    class Ej36
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello ");
+        }
+    }
+}
